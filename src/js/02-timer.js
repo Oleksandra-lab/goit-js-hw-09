9754d5ada037918d8622ea54768289f964d02808
Oleksandra-lab <tka4olex@gmail.com>
2023-08-05 16:41:33 +0300

    task 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,27 +1,64 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-const dataInput = document.getElementById(datetime-picker);
-const btnStart = document.querySelector('button[data-start]');
-const daysLeft = document.querySelector('[data-days]')
-const hoursLeft = document.querySelector('[data-hours]')
-const minutesLeft = document.querySelector('[data-minutes]')
-const secondsLeft = document.querySelector('[data-seconds]')
+const dataInputEl = document.getElementById("datetime-picker");
+const btnStartEl = document.querySelector('button[data-start]');
+const timerEl = document.querySelector('.timer')
+const daysEl = timerEl.querySelector('[data-days]')
+const hoursEl = timerEl.querySelector('[data-hours]')
+const minutesEl = timerEl.querySelector('[data-minutes]')
+const secondsEl = timerEl.querySelector('[data-seconds]')
 
-btnStart.addEventListener('click', startTimer)
+btnStartEl.addEventListener('click', startTimer)
+let intId = null;
 
+function padStart(value){
+  return value.toString().padStart(2, "0")
+
+}
+
+function startTimer(){
+  clearInterval(intId)
+let deltaTime = new Date(dataInputEl.value) - Date.now();
+ intId = setInterval(()=>{
+  deltaTime -= 1000;
+  if(deltaTime < 1000){
+    clearInterval(intId);
+    updateTimer();
+    return
+  } 
+  const date = convertMs(deltaTime)
+  updateTimer(date)
+}, 1000)
+}
+function updateTimer({ days = "00", hours = "00", minutes = "00", seconds = "00"} = {}){
+  
+  daysEl.textContent = padStart(days);
+  hoursEl.textContent = padStart(hours);
+  minutesEl.textContent = padStart(minutes);
+  secondsEl.textContent = padStart(seconds);
+
+
+}
+btnStartEl.disabled = true;
 
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
+    onClose([selectedDates]) {
+      const toggle = selectedDates < Date.now()
+      btnStartEl.disabled = toggle;
+      if(toggle){
+        return alert("Please choose a date in the future");
+      }
     },
   };
 
 
+
+flatpickr(dataInputEl, options)
 
 
   function convertMs(ms) {
