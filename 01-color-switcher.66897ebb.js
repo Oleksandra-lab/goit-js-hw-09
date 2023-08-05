39cf58db68d89ed0bfccfc0613f45984bc33f51d
Oleksandra-lab <127051632+Oleksandra-lab@users.noreply.github.com>
2023-08-05 06:14:37 +0000

    Deploying to gh-pages from @ Oleksandra-lab/goit-js-hw-09@6eb27ab9ab413463e67b533b699cae9dc9fac63b 🚀

---
+++ 01-color-switcher.66897ebb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop");t.addEventListener("click",onClick),e.addEventListener("click",offClick)}();
+//# sourceMappingURL=01-color-switcher.66897ebb.js.map
