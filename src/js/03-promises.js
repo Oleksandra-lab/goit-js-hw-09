a94a674bf9ac8968ac7d999a4ef0a762db280bac
Oleksandra-lab <tka4olex@gmail.com>
2023-08-05 15:00:25 +0300

    task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,36 @@
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const form = document.querySelector('.form');
-const delay = document.querySelector('[name="delay"]');
-const step = document.querySelector('[name="step"]');
-const amount = document.querySelector('[name="amount"]');
+const formEl = document.querySelector('.form');
 
-form.addEventListener('click', )
+formEl.addEventListener('submit', onSubmit)
 
+function onSubmit(evt){
+  evt.preventDefault();
+  const {amount, delay, step} = evt.target.elements
+  const amountValue = Number(amount.value)
+  let delayValue = Number(delay.value)
+  const stepValue = Number(step.value)
+  for (let i = 0; i < amountValue; i +=1){
+createPromise(i +1, delayValue)
+.then(({position, delay})=>{ Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);})
+.catch(({position, delay})=>{ Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);})
+delayValue += stepValue
+  }
+}
 
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  
+  return new Promise((res, rej)=>{
+    setTimeout(()=>{
+      if (shouldResolve) {
+        res({position, delay})
+      } else {
+        rej({position, delay})
+      }
+    },delay 
+    )
+  })
 }
